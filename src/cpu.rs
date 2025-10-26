32c7fbb6a320ee76c9afdafae334858586a1c9b6
Anne Castrillon <anne.castrillon@gmail.com>
2025-10-25 18:58:59 -0700

    minor changes

--- src/cpu.rs
+++ src/cpu.rs
@@ -263,7 +263,7 @@ impl CPU {
         for i in 0..self.bus.timers.len() {
             let timer = &mut self.bus.timers[i];
 
-            if timer.is_active && timer.clock_source != ClockSource::Hblank && timer.clock_source != ClockSource::DotClock {
+            if timer.is_active && [ClockSource::SystemClock, ClockSource::SystemClockDiv8].contains(&timer.clock_source) {
                 timer.tick(cycles, &mut self.bus.interrupt_stat);
             }
         }
