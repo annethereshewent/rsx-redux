b0712db4ad0dfee516cbe7b8a836b94776f5dc32
Anne Castrillon <anne.castrillon@gmail.com>
2026-06-11 20:21:58 -0700

    add fragment and vertex shaders

--- renderer-webgl/src/renderer.rs
+++ renderer-webgl/src/renderer.rs
@@ -1,6 +1,6 @@
 use js_sys::wasm_bindgen::JsCast;
 use rsx_redux::cpu::bus::gpu::{CPUTransferParams, DisplayDepth, FillVramParams, GPUCommand, Polygon, TexturePageColors, VRamTransferParams, VramToVramTransferParams, GPU};
-use web_sys::{HtmlCanvasElement, WebGl2RenderingContext, WebGlTexture};
+use web_sys::{window, HtmlCanvasElement, WebGl2RenderingContext, WebGlProgram, WebGlShader, WebGlTexture};
 
 pub struct Renderer {
     canvas: HtmlCanvasElement,
@@ -11,7 +11,7 @@ pub struct Renderer {
 
 impl Renderer {
     pub fn new(canvas_id: &str) -> Self {
-        let document = web_sys::window().unwrap().document().unwrap();
+        let document = window().unwrap().document().unwrap();
         let canvas = document
             .get_element_by_id(canvas_id)
             .unwrap()
@@ -28,11 +28,82 @@ impl Renderer {
         let vram_read = gl.create_texture().unwrap();
         let vram_write = gl.create_texture().unwrap();
 
+        let fragment_shader_str = include_str!("shaders/fragment.glsl");
+        let vertex_shader_str = include_str!("shaders/vertex.glsl");
+
+        let fragment_shader = Self::compile_shader(
+            &gl,
+            WebGl2RenderingContext::FRAGMENT_SHADER,
+            fragment_shader_str
+        ).unwrap();
+        let vertex_shader = Self::compile_shader(
+            &gl,
+            WebGl2RenderingContext::VERTEX_SHADER,
+            vertex_shader_str
+        ).unwrap();
+
+        let program = Self::link_program(&gl, &vertex_shader, &fragment_shader).unwrap();
+
+        gl.use_program(Some(&program));
+
         Self {
             canvas,
             gl,
             vram_read,
             vram_write,
+        }
+    }
+
+    fn compile_shader(
+        gl: &WebGl2RenderingContext,
+        shader_type: u32,
+        source: &str
+    ) -> Result<WebGlShader, String> {
+        let shader = gl
+            .create_shader(shader_type)
+            .ok_or("Unable to create shader object".to_string())?;
+
+        gl.shader_source(&shader, source);
+        gl.compile_shader(&shader);
+
+        if gl
+            .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(shader)
+        } else {
+            Err(gl
+                .get_shader_info_log(&shader)
+                .unwrap_or("unknown error creating shader".to_string())
+            )
+        }
+    }
+
+    fn link_program(
+        gl: &WebGl2RenderingContext,
+        vertex_shader: &WebGlShader,
+        fragment_shader: &WebGlShader,
+    ) -> Result<WebGlProgram, String> {
+        let program = gl
+            .create_program()
+            .ok_or("Unable to create gl program")?;
+
+        gl.attach_shader(&program, vertex_shader);
+        gl.attach_shader(&program, fragment_shader);
+        gl.link_program(&program);
+
+        if gl
+            .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(program)
+        } else {
+            Err(gl
+                .get_program_info_log(&program)
+                .unwrap_or("Unable to create gl program for unknown reason".to_string())
+            )
         }
     }
 
---
+++ renderer-webgl/src/shaders/fragment.glsl
@@ -0,0 +1,6 @@
+precision highp float;
+out vec4 outColor;
+
+void main() {
+    outColor = vec4(1, 0, 0, 1);
+}
